3eea746798322bf9eb0098f98410c324464f56a3
pedro772 <plucena17230@gmail.com>
2022-01-21 18:51:26 -0300

    Estilização do botão de cadastro e renderização de componente Card apenas com título válido

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,18 @@ import React from "react";
 
 export default function Card(props) {
     return (
+        props.title &&
         <section className="card">
             <img src={props.imageUrl} alt={props.title} className="card--image"/>
             <div className="card--info">
                 <div className="card--info-location">
                     <img src="/images/pin.png" alt="pin" className="card--location-pin" />
-                    <label className="card--location-text">{props.location}</label>
+                    {props.location && <label className="card--location-text">{props.location}</label>}
                     {props.googleMapsUrl && <a href={props.googleMapsUrl} target="_blank" rel="noopener noreferrer" className="card--location-link">View on Google Maps</a>}
                 </div>
-                <h2 className="card--title">{props.title}</h2>
-                <h6 className="card--dates">{props.startDate} - {props.endDate}</h6>
-                <p className="card--description">{props.description}</p>
+                {props.title && <h2 className="card--title">{props.title}</h2>}
+                {props.startDate && props.endDate && <h6 className="card--dates">{props.startDate} - {props.endDate}</h6>}
+                {props.description && <p className="card--description">{props.description}</p>}
             </div>
         </section>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <img src="/images/nav-icon.png" alt="Globe Icon" className="navbar--icon" />
-            <h1 className="navbar--header">my travel destinations.</h1>
+            <label className="navbar--header">my travel destinations.</label>
         </nav>
     );
 }
